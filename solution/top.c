c8c5cadbcdd799a329f48169a01b6bcf5e2b4645
TukashovaAknur <57065311+TukashovaAknur@users.noreply.github.com>
2019-11-19 02:03:42 +0600

    Update top.c

--- solution/top.c
+++ solution/top.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 
 int main{
-  // your solution for splab3
+ "$(cut -d ' ' -f7 log.txt | awk '{
+    list[$1]+=$2
+   }
+   END {
+     for (i in list) printf("%s\t%s\n",i,list[i])
+   }' | sort -r -n -k 2 | head -n 5 )"}'
+   
 }
